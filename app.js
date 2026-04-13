fb8b8bc447d085b53956afd8646cbc3d0a8bff22
zullytamayom <zullytamayom@gmail.com>
2026-04-13 10:06:11 -0500

    js

---
+++ app.js
@@ -0,0 +1,13 @@
+const btnBuscar = document.getElementById("btnBuscar");
+const mostrarPokemon = document.getElementById("mostrarPokemon");
+const btnFavoritos = document.getElementById("btnFavoritos");
+const mostrarFavoritos = document.getElementById("mostrarFavoritos");
+
+
+btnBuscar.addEventListener("click",function(){
+
+   const nombre = document.getElementById("input").value;
+
+   console.log(nombre)
+})
+
