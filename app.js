5baedc856e2783dd4bfaa75c123eb12d54a26703
KarenDiaz <calidad8844@gmail.com>
2026-04-13 10:26:12 -0500

    funcion buscar pokemon

--- app.js
+++ app.js
@@ -4,10 +4,31 @@ const btnFavoritos = document.getElementById("btnFavoritos");
 const mostrarFavoritos = document.getElementById("mostrarFavoritos");
 
 
-btnBuscar.addEventListener("click",function(){
+// btnBuscar.addEventListener("click",function(){
 
-   const nombre = document.getElementById("input").value;
+//    const nombre = document.getElementById("input").value;
 
-   console.log(nombre)
-})
+//    console.log(nombre)
+// })
+
+btnBuscar.addEventListener("click", function () {
+
+  const nombre = document.getElementById("input").value.toLowerCase();
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
+    .then(res => res.json())
+    .then(data => {
+
+      console.log(data); //revisar
+
+      mostrarPokemon.innerHTML = `
+        <h3>${data.name}</h3>
+        <img src="${data.sprites.front_default}">
+      `;
+    })
+    .catch(() => {
+      alert("Pokemun no encontrado");
+    });
+
+});
 
